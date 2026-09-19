9cfd64eaebc8ea3567ef3a4dd3145706ff3020e3
peterzhenhh <peterjs001@gmail.com>
2026-09-19 19:10:38 +0800

    fix: improve code readability and consistency in getCode function

--- src/utils/codeReader.ts
+++ src/utils/codeReader.ts
@@ -2,21 +2,19 @@ import codeMapping from "../types/codeMapping";
 const invMap = (obj: Record<string, any>) => {
     return Object.fromEntries(Object.entries(obj).map(([k, v]) => [v, k]));
 };
-export const getCode = (codePrefix: number, type: "work" | "group"): string => {
-    if (codePrefix >= 0) {
-        return "";
+export const getCode = (numPrefix: number, type: "work" | "group"): string => {
+    let codePrefix = numPrefix.toString();
+    if (parseInt(codePrefix) >= 0) {
+        codePrefix = "";
     }
     const mapping: Record<string, string> = invMap(codeMapping[type]);
     if (!Object.hasOwn(mapping, codePrefix)) {
         throw new Error(`${mapping}-${type} has no key named ${codePrefix}`);
     }
-    return mapping[codePrefix];
+    return `${mapping[codePrefix]}${parseInt(codePrefix) >= 0 ? parseInt(codePrefix).toString() : numPrefix.toString()}`;
 };
 
-export const getNum = (
-    numPrefix: string,
-    type: "work" | "group",
-): number | "" => {
+export const getNum = (numPrefix: string, type: "work" | "group"): number => {
     const mapping: Record<string, any> = codeMapping[type];
     if (!Object.hasOwn(mapping, numPrefix)) {
         throw new Error(`${mapping}-${type} has no key named ${numPrefix}`);
